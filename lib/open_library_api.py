c33ab81f6ab756880a8b5cec9600541b499da6c1
Yuriamani25 <yuriamani7@gmail.com>
2024-07-29 06:58:00 +0300

    working with apis

--- lib/open_library_api.py
+++ lib/open_library_api.py
@@ -34,11 +34,14 @@ class Search:
 
     def get_user_search_results(self, search_term):
         search_term_formatted = search_term.replace(" ", "+")
+        print(search_term_formatted)
         fields = ["title", "author_name"]
         fields_formatted = ",".join(fields)
+        print(fields_formatted)
         limit = 1
 
         URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
+        print(URL)
 
         response = requests.get(URL).json()
         response_formatted = f"Title: {response['docs'][0]['title']}\nAuthor: {response['docs'][0]['author_name'][0]}"
